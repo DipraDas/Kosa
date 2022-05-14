a6d59d6e3dbe2ecf3990588498dc1e70a8f60fac
Dipra Das <dipradas5940@gmail.com>
2022-05-14 20:37:59 +0600

    About 4 Sections Added

--- src/components/Pages/About/AboutComponents/AboutComponents.js
+++ src/components/Pages/About/AboutComponents/AboutComponents.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import ClientLogo from '../../Home/ClientLogo/ClientLogo';
+import AboutHairstylist from '../About Hairstylist/AboutHairstylist';
 import AboutBanner from '../AboutBanner/AboutBanner';
+import AboutJoinOurTeam from '../AboutJoinOurTeam/AboutJoinOurTeam';
 import AboutMarketing from '../AboutMarketing/AboutMarketing';
+import AboutOpeningHour from '../AboutOpeningHour/AboutOpeningHour';
 import './AboutComponents.css';
 
 const AboutComponents = () => {
@@ -8,6 +12,10 @@ const AboutComponents = () => {
         <div>
             <AboutBanner></AboutBanner>
             <AboutMarketing></AboutMarketing>
+            <AboutHairstylist></AboutHairstylist>
+            <ClientLogo></ClientLogo>
+            <AboutOpeningHour></AboutOpeningHour>
+            <AboutJoinOurTeam></AboutJoinOurTeam>
         </div>
     );
 };
